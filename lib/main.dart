d22dc4816cd5ba86a00ff4b567444cc07d2e2fb7
bit00 <akbrijesh@gmail.com>
2020-08-28 21:24:18 +0530

    minor

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'dart:async';
 import 'dart:convert';
 
+import 'package:path_provider/path_provider.dart';
+
 void main() {
   runApp(ReadCsv());
 }
@@ -84,3 +86,38 @@ class _ReadCsvHomePageState extends State<ReadCsvHomePage> {
     );
   }
 }
+
+class CounterStorage {
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/counter.txt');
+  }
+
+  Future<int> readCounter() async {
+    try {
+      final file = await _localFile;
+
+      // Read the file
+      String contents = await file.readAsString();
+
+      return int.parse(contents);
+    } catch (e) {
+      // If encountering an error, return 0
+      return 0;
+    }
+  }
+
+  Future<File> writeCounter(int counter) async {
+    final file = await _localFile;
+
+    // Write the file
+    return file.writeAsString('$counter');
+  }
+}
+
